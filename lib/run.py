deeee3a421438ed2e176ec73d7f7f5a049328bf6
Luigi Federico <luigi.fede.22@gmail.com>
2022-12-30 23:44:05 +0100

    piccoli ritocchi

--- lib/run.py
+++ lib/run.py
@@ -1,12 +1,30 @@
 from data import MVTecDataset, mvtec_classes
-from typing import List
+from patch_core import PatchCore
 
 ALL_CLASSES = mvtec_classes()
 
 
-def run_model(classes: List = ALL_CLASSES):
+def run_model(
+    classes: list = ALL_CLASSES, 
+    f_coreset: float = 0.1,
+):
+    print(f'Running PatchCore...')
     for cls in classes:
-        train_ds, test_ds = MVTecDataset(cls).get_dataloaders()
+        train_dl, test_dl = MVTecDataset(cls).get_dataloaders()
+        patch_core = PatchCore(f_coreset)
+
+        print(f'Class {cls}:')
+        print(f'  Training...')
+        patch_core.fit(train_dl)
+        print(f'Testing...')
+        # evaluate..
+
+        # Print test results
+        # Save test results
+    
+    # Save global results
+
+    # Return global results
 
 
 if __name__ == "__main__":
