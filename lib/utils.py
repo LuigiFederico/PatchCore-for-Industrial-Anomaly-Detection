3e22856eac5c9797c63557768e83b87777aee44f
Alex Carluccio <58367339+LordAssalt@users.noreply.github.com>
2023-01-03 17:15:13 +0100

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -72,7 +72,6 @@ def gaussian_blur(img: tensor) -> tensor:
     max_value = img.max()  # Maximum value of all elements in the image tensor
     blurred_pil = tensor_to_pil(img[0] / max_value).filter(blur_kernel)
     blurred_map = pil_to_tensor(blurred_pil) * max_value
-    plt.imshow(blurred_pil)
     
     return blurred_map 
     
