Fix CLIP scaling factor

lib/patch_core.py
@@ -2,10 +2,12 @@ import torch
 import random
 import clip  #needed for CLIP
 import numpy as np
+import torchvision
 import torchvision.transforms as T
 from tqdm import tqdm
 from torch import tensor
 from torch.utils.data import DataLoader
+from torch.nn import functional as F
 from sklearn.metrics import roc_auc_score
 from .utils import gaussian_blur, get_coreset
 from PIL import Image  #needed for CLIP
@@ -37,7 +39,7 @@ class PatchCore(torch.nn.Module):
         print(f"Net Used {backbone}")
 
         if vanilla==True:
-            self.model = torch.hub.load('pytorch/vision', 'wide_resnet50_2', pretrained=True)
+            self.model = torch.hub.load('pytorch/vision:v0.13.0', 'wide_resnet50_2', pretrained=True)
             self.model.layer2[-1].register_forward_hook(hook)  # Register hooks
             self.model.layer3[-1].register_forward_hook(hook)  # Register hooks
         else: